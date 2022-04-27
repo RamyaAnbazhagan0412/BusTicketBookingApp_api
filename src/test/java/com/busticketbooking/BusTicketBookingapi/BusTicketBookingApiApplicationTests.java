bcacb86f8ac9d25844b787d04a6de1ae3e2e8613
RamyaAnbazhagan0412 <aramya0412@gmail.com>
2022-04-27 17:47:06 +0530

    first commit

---
+++ src/test/java/com/busticketbooking/BusTicketBookingapi/BusTicketBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busticketbooking.BusTicketBookingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
